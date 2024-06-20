b82742ed86d754e2cbc2b5d181cb7d9d8f74a646
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-06-20 18:33:23 +0500

    practice

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int[] numbers = {2, 4, 6, 7, 5, 8, 10};
+    }
+}
---
+++ src/Stacks.java
@@ -0,0 +1,7 @@
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+
+    }
+}
