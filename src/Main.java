c88e6e232bd6718b3ffb1a1e9ccdf07cb0650664
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-06-22 22:14:28 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        boolean isSorted = true;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Sorted");
+        } else {
+            System.out.println("Not sorted");
+        }
     }
 }
