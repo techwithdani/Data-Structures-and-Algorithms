16f5b499c76331b40d1010c4aab26ab8f930eba3
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-07-01 21:15:32 +0500

    practicing

--- src/Stacks.java
+++ src/Stacks.java
@@ -1,5 +1,19 @@
+import java.util.Stack;
 public class Stacks {
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
 
+        stack.push(5);
+        stack.push(4);
+        stack.push(7);
+        stack.push(9);
+        stack.push(10);
+
+        int size = stack.size();
+
+        for(int i = 1; i <= size; i++) {
+            System.out.println(stack.pop());
+        }
+        
     }
 }
